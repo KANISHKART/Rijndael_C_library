bd8f8808a7a12222057f3253210cc619e5c35ed3
kanishkart <kanishkar.thiruna@gmail.com>
2024-03-26 21:14:35 +0000

    Edited with comments

--- rijndael.c
+++ rijndael.c
@@ -94,68 +94,76 @@ void add_round_key(unsigned char *block, unsigned char *round_key) {
     block[i]^=round_key[i];
   }
 }
-
+ 
 /*
  * This function should expand the round key. Given an input,
  * which is a single 128-bit key, it should return a 176-byte
  * vector, containing the 11 round keys one after the other
  */
 unsigned char *expand_key(unsigned char *cipher_key) {
+
+  //setting size to accomodate 11 round keys
   unsigned char *output = (unsigned char *)malloc(BLOCK_SIZE * 11);
+
   int rcon_index=1;
+
+  //to iterate each rows
   int row=0;
+
+  //starting point of value
   int itr_index= BLOCK_SIZE;
+
+  //base pointer on last row in master key
   int lastRow=3;
+
+  //iterating 11 times to expand the keys
   for(int j=0;j<11;j++){
+
     //copying all key to expand_key array
     if(j==0){
         for(int i=0;i<16;i++)
           output[i]=cipher_key[i];
     }
+
+    //expanding logic for remaining 10 keys from previous one.
     else{
-      //circular switch
+
+      //Circular switch for ROT word i.e., last column in the current key
       int rot[4] = {BLOCK_ACCESS(output,lastRow,1),BLOCK_ACCESS(output,lastRow,2),BLOCK_ACCESS(output,lastRow,3),BLOCK_ACCESS(output,lastRow,0)};
-      //Replace rot array using sub bytes S-Box lookup table
-      
+     
+      //Replace ROT array using SubBytes S-Box lookup table
       for(int i=0;i<4;i++){
         rot[i]=sbox[rot[i]];
-        // printf("%d\t",rot[i]);
       }
-        // printf("\n");
+
+      // iterate each key matrix (4 X 4) here in the loop
       for(int k=0;k<4;k++){
-        // For 1st column for new key -> XOR with 1st column in previous 16 bytes ,  Rcon , sub bytes array
-        if(k==0){
-          output[itr_index] = BLOCK_ACCESS(output,row,0) ^ rcon[rcon_index++] ^ rot[0];
+        
+        if(k==0){ // last row ⊕ RCON ⊕ ROT  
+          output[itr_index]   = BLOCK_ACCESS(output,row,0) ^ rcon[rcon_index++] ^ rot[0]; // Rcon XOR for rest is 0 so, XOR the 1st column
           output[itr_index+1] = BLOCK_ACCESS(output,row,1) ^ rot[1];
           output[itr_index+2] = BLOCK_ACCESS(output,row,2) ^ rot[2];
           output[itr_index+3] = BLOCK_ACCESS(output,row,3) ^ rot[3];
-          printf("XOR1::%d\t%d\t%d\t%d\t\n",BLOCK_ACCESS(output,row,0),BLOCK_ACCESS(output,row,1),BLOCK_ACCESS(output,row,2),BLOCK_ACCESS(output,row,3));
-          printf("XOR2::%d\t%d\t%d\t%d\t\n",rot[0],rot[1],rot[2],rot[3]);
-
-          printf("Res::%d\t%d\t%d\t%d\t\n",output[itr_index],output[itr_index+1],output[itr_index+2],output[itr_index+3]);
-
           row+=1;
           itr_index+=4;
-          printf("----------------------------------------------------------\n");
-
         }
-        else{
+        else{ // previous row ⊕ similar previous key row
           output[itr_index]   = BLOCK_ACCESS(output,row,0) ^ output[itr_index-4]; 
           output[itr_index+1] = BLOCK_ACCESS(output,row,1) ^ output[itr_index-3];
           output[itr_index+2] = BLOCK_ACCESS(output,row,2) ^ output[itr_index-2];
           output[itr_index+3] = BLOCK_ACCESS(output,row,3) ^ output[itr_index-1];
-          printf("XOR1::%d\t%d\t%d\t%d\t\n",BLOCK_ACCESS(output,row,0),BLOCK_ACCESS(output,row,1),BLOCK_ACCESS(output,row,2),BLOCK_ACCESS(output,row,3));
-          printf("XOR2::%d\t%d\t%d\t%d\t\n",output[itr_index-4],output[itr_index-3],output[itr_index-2],output[itr_index-1]);
-
-          printf("Res::%d\t%d\t%d\t%d\t\n",output[itr_index],output[itr_index+1],output[itr_index+2],output[itr_index+3]);
-          printf("----------------------------------------------------------\n");
+    
           itr_index+=4;
           row+=1;
         }
       }
+
+      //updating base pointer to recently generated key 
       lastRow+=4;
     }
+    
   }
+
   return output;
 }
 
