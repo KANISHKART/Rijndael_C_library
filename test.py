423fe3f95ef37304c034a9e82d0015f334b6b72f
kanishkart <kanishkar.thiruna@gmail.com>
2024-04-08 23:50:07 +0000

    Added mix colums , round key unit test

--- test.py
+++ test.py
@@ -24,6 +24,8 @@ c_aes.sub_bytes.argtypes=[ctypes.POINTER(ctypes.c_ubyte),ctypes.c_int]
 c_aes.invert_sub_bytes.argtypes=[ctypes.POINTER(ctypes.c_ubyte),ctypes.c_int]
 c_aes.shift_rows.argtypes=[ctypes.POINTER(ctypes.c_ubyte)]
 c_aes.invert_shift_rows.argtypes=[ctypes.POINTER(ctypes.c_ubyte)]
+c_aes.add_round_key.argtypes=[ctypes.POINTER(ctypes.c_ubyte),ctypes.POINTER(ctypes.c_ubyte),ctypes.c_int,ctypes.c_int]
+c_aes.mixColumns.argtypes=[ctypes.POINTER(ctypes.c_ubyte)]
 
 # the below line sets the response type for the aes_encrypt_block function
 c_aes.aes_encrypt_block.restype = ctypes.POINTER(ctypes.c_ubyte)
@@ -33,6 +35,8 @@ c_aes.sub_bytes.restype= ctypes.c_voidp
 c_aes.invert_sub_bytes.restype= ctypes.c_voidp
 c_aes.shift_rows.restype= ctypes.c_voidp
 c_aes.invert_shift_rows.restype= ctypes.c_voidp
+c_aes.add_round_key.restype= ctypes.c_voidp
+c_aes.mixColumns.restype= ctypes.c_voidp
 
 
 class TestEncrypt(unittest.TestCase):
@@ -118,6 +122,37 @@ class TestEncrypt(unittest.TestCase):
         
         self.assertEqual(list(plaintext_c),list(plaintext_py))
 
+    def test_add_round_key(self):
+    
+        plaintext_c= b'\x32\x14\x2E\x56\x33\x09\x46\x1A\x4B\x11\x13\x11\x04\x08\x06\x63'
+        key_c=b'\x32\x14\x2E\x56\x43\x09\x46\x1B\x4B\x11\x33\x11\x04\x08\x06\x63'
+        
+        plaintext_py= b'\x32\x14\x2E\x56\x33\x09\x46\x1A\x4B\x11\x13\x11\x04\x08\x06\x63'
+        key_py=b'\x32\x14\x2E\x56\x43\x09\x46\x1B\x4B\x11\x33\x11\x04\x08\x06\x63'
+        
+        plaintext_arr = (ctypes.c_ubyte * len(plaintext_c))(*plaintext_c)
+        key_arr = (ctypes.c_ubyte * len(key_c))(*key_c)
+        
+        c_aes.add_round_key(plaintext_arr, key_arr ,0,15)
+        
+        aes.add_round_key(aes.bytes2matrix(plaintext_py), aes.bytes2matrix(key_py))
+        
+        self.assertEqual(list(plaintext_c),list(plaintext_py)) 
+        
+    def test_mixColumns(self):
+    
+        plaintext_c= b'\x32\x14\x2E\x56\x33\x09\x46\x1A\x4B\x11\x13\x11\x04\x08\x06\x63'
+    
+        plaintext_py= b'\x32\x14\x2E\x56\x33\x09\x46\x1A\x4B\x11\x13\x11\x04\x08\x06\x63'
+        
+        plaintext_arr = (ctypes.c_ubyte * len(plaintext_c))(*plaintext_c)
+        
+        c_aes.mixColumns(plaintext_arr)
+        
+        aes.mix_columns(aes.bytes2matrix(plaintext_py))
+        
+        self.assertEqual(list(plaintext_c),list(plaintext_py)) 
+        
     def test_c_aes_encrypt_block(self):
         
         plaintext= b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F'
